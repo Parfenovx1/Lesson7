d0c5eab6625b36f1064229c1ca4eeba17df0c61c
Parfenovx1 <parfenovx1@gmail.com>
2020-02-28 05:37:06 +0200

    commit7

---
+++ src/index.js
@@ -0,0 +1,32 @@
+var user;
+var number;
+function getRandomNumber(){
+  number = Number(Math.ceil(Math.random() * 10));
+  console.log(number);
+}
+getRandomNumber();   // подсказка, что бы долго не сидеть 
+
+function tryToGuess(){
+  user =  Number(prompt("Try to guess a number from 1 to 10:"));
+}
+
+function isNumbersCoincide(){
+  do{
+    tryToGuess();
+    if(number == user){
+      console.clear();
+      console.log("Correct!!!");
+      return;
+    }
+    if(user === null){
+      console.clear();
+      console.log("You canceled the game.")
+      return;
+      }
+    console.clear()
+    console.log("Wrong. Try again!")
+    tryToGuess();
+  }while(user !== number)
+} 
+isNumbersCoincide();
+
